ed5fc0160b1c622258750e597a64accd3d962801
Aleksejs Birula <ice19942335@gmail.com>
2025-12-13 21:40:22 +0200

    Styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
 function Header() {
   return (
     <header className="header">
+      <div className="company-name">ROLLEG</div>
       <h1>🏒 Турнирная таблица по хоккею</h1>
       <p>Управление командами, играми и результатами турнира</p>
     </header>
