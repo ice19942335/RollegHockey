13154eb4e867e5b56fde017f3302976777464987
Aleksejs Birula <ice19942335@gmail.com>
2025-12-13 22:34:53 +0200

    Stules game card for small

--- src/components/GameCard.jsx
+++ src/components/GameCard.jsx
@@ -9,6 +9,9 @@ const GAME_TYPE_LABELS = {
 function GameCard({ game, homeTeam, awayTeam, onDelete }) {
   if (!homeTeam || !awayTeam) return null
 
+  const homeTeamColor = homeTeam?.color || '#FFA000'
+  const awayTeamColor = awayTeam?.color || '#FFA000'
+
   return (
     <div className="game-card">
       <button 
@@ -22,18 +25,24 @@ function GameCard({ game, homeTeam, awayTeam, onDelete }) {
           <span className="game-logo">
             <TeamLogo logo={homeTeam.logo} name={homeTeam.name} />
           </span>
-          <span>{homeTeam.name}</span>
+          <span style={{ color: homeTeamColor }}>{homeTeam.name}</span>
         </div>
-        <div className="game-score-display">
-          <span className="score">{game.homeScore}</span>
+        <div className="game-score-display game-score-display-desktop">
+          <span className="game-logo-inline">
+            <TeamLogo logo={homeTeam.logo} name={homeTeam.name} />
+          </span>
+          <span className="score" style={{ color: homeTeamColor }}>{game.homeScore}</span>
           <span className="score-separator">:</span>
-          <span className="score">{game.awayScore}</span>
+          <span className="score" style={{ color: awayTeamColor }}>{game.awayScore}</span>
+          <span className="game-logo-inline">
+            <TeamLogo logo={awayTeam.logo} name={awayTeam.name} />
+          </span>
         </div>
         <div className="game-team game-team-away">
           <span className="game-logo">
             <TeamLogo logo={awayTeam.logo} name={awayTeam.name} />
           </span>
-          <span>{awayTeam.name}</span>
+          <span style={{ color: awayTeamColor }}>{awayTeam.name}</span>
         </div>
       </div>
       <div className="game-info">
