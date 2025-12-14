48cbe3261c4e0a4c23e5cd9dc1778a3fb5e96d89
Aleksejs Birula <ice19942335@gmail.com>
2025-12-14 19:28:08 +0200

    Add happy-dom for testing environment and update dependencies

--- vite.config.js
+++ vite.config.js
@@ -13,7 +13,7 @@ export default defineConfig({
   },
   test: {
     globals: true,
-    environment: 'jsdom',
+    environment: 'happy-dom',
     setupFiles: './src/test/setup.js',
     pool: 'threads',
     poolOptions: {
